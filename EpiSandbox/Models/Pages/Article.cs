ab541239879831203304d5cd53a3a06f4e410ec4
Mikael Lindh <mslindh@gmail.com>
2021-01-12 15:45:12 +0100

    Added dummy content

---
+++ EpiSandbox/Models/Pages/Article.cs
@@ -0,0 +1,29 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EpiSandbox.Models.Pages
+{
+    [ContentType(DisplayName = "Article", GUID = "5d5d8a95-9060-4069-abcf-ec26df60a6cd", Description = "")]
+    public class Article : PageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Heading",
+            Description = "Heading",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
