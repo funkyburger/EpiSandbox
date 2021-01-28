52f71b8ca8004074f4084b4b91751ff7b53c9951
Mikael Lindh <mikael.lindh.konsult@gmail.com>
2021-01-28 22:44:33 +0100

    Added search suggestion.

---
+++ EpiSandbox/Controllers/SearchApiController.cs
@@ -0,0 +1,45 @@
+﻿using EpiSandbox.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EpiSandbox.Controllers
+{
+    public class SearchApiController : ApiController
+    {
+        [Route("search")]
+        public IEnumerable<SearchHit> Get(string query, int page = 1, int pageSize = 20)
+        {
+            // TODO : real search
+            return new SearchHit[] { 
+                new SearchHit() 
+                {
+                    Headline = "Toto",
+                    Sample = "asdasddsaasd asd sad sad dsa  asd dsa",
+                    Link = "/Blah/blih/bluh"
+                },
+                new SearchHit()
+                {
+                    Headline = "Titi",
+                    Sample = "asdasddsaasd asd sad sad dsa  asd dsa",
+                    Link = "/Blah/blih/bluh"
+                },
+                new SearchHit()
+                {
+                    Headline = "Tata",
+                    Sample = "asdasddsaasd asd sad sad dsa  asd dsa",
+                    Link = "/Blah/blih/bluh"
+                },
+                new SearchHit()
+                {
+                    Headline = "Page with long headline and stuff",
+                    Sample = "asdasddsaasd asd sad sad dsa  asd dsa",
+                    Link = "/Blah/blih/bluh"
+                }
+            };
+        }
+    }
+}
