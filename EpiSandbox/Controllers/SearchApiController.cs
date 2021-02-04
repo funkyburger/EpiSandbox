bfa92bb32361e4e3a2dce9f9c4653c624b050788
Mikael Lindh <mslindh@gmail.com>
2021-02-04 15:25:38 +0100

    Changed mock search.

--- EpiSandbox/Controllers/SearchApiController.cs
+++ EpiSandbox/Controllers/SearchApiController.cs
@@ -13,33 +13,18 @@ namespace EpiSandbox.Controllers
         [Route("api/search")]
         public IEnumerable<SearchHit> Get(string query, int page = 1, int pageSize = 20)
         {
-            // TODO : real search
-            return new SearchHit[] { 
-                new SearchHit() 
+            if(page < 5)
+            {
+                for (int i = 0; i < pageSize; i++)
                 {
-                    Headline = "Toto",
-                    Sample = "asdasddsaasd asd sad sad dsa  asd dsa",
-                    Link = "/Blah/blih/bluh"
-                },
-                new SearchHit()
-                {
-                    Headline = "Titi",
-                    Sample = "asdasddsaasd asd sad sad dsa  asd dsa",
-                    Link = "/Blah/blih/bluh"
-                },
-                new SearchHit()
-                {
-                    Headline = "Tata",
-                    Sample = "asdasddsaasd asd sad sad dsa  asd dsa",
-                    Link = "/Blah/blih/bluh"
-                },
-                new SearchHit()
-                {
-                    Headline = "Page with long headline and stuff",
-                    Sample = "asdasddsaasd asd sad sad dsa  asd dsa",
-                    Link = "/Blah/blih/bluh"
+                    yield return new SearchHit()
+                    {
+                        Headline = "Lorem ipsum",
+                        Sample = "On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes).",
+                        Link = "/Blah/blih/bluh"
+                    };
                 }
-            };
+            }
         }
     }
 }
