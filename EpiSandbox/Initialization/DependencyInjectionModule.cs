e34a2f1f2dc53c580b2da0929e662f421808aee5
Mikael Lindh <mikael.lindh.konsult@gmail.com>
2021-02-09 09:16:18 +0100

    Enabled DI for api controllers.

--- EpiSandbox/Initialization/DependencyInjectionModule.cs
+++ EpiSandbox/Initialization/DependencyInjectionModule.cs
@@ -3,7 +3,10 @@ using EPiServer.Framework.Initialization;
 using EPiServer.ServiceLocation;
 using System;
 using System.Linq;
+using System.Web.Http;
 using System.Web.Mvc;
+using Unity;
+using Unity.WebApi;
 
 namespace EpiSandbox.Initialization
 {
@@ -12,7 +15,19 @@ namespace EpiSandbox.Initialization
     {
         public void ConfigureContainer(ServiceConfigurationContext context)
         {
+            var container = new UnityContainer();
+            container.RegisterType<Data.ILinkService, Data.LinkService>();
+            container.RegisterType<Data.IPageSearcher, Data.PageSearcher>();
+
+            //GlobalConfiguration.Configuration.DependencyResolver
+            //GlobalConfiguration.Configuration.DependencyResolver = 
+            GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
+
+            // TODO One container to rule them all
+            //DependencyResolver.SetResolver(container);
+
             context.Services.AddTransient<Data.ILinkService, Data.LinkService>();
+            context.Services.AddTransient<Data.IPageSearcher, Data.PageSearcher>();
         }
 
         public void Initialize(InitializationEngine context)
@@ -24,4 +39,9 @@ namespace EpiSandbox.Initialization
         {
         }
     }
+
+    //public class Blah
+    //{
+
+    //}
 }
