3c5749353d3643f95f9755f1a43160c21e24aeb8
Mikael Lindh <mslindh@gmail.com>
2021-03-01 10:27:28 +0100

    Moved js to typescript.

---
+++ EpiSandbox/Content/Scripts/Navbar/NavbarHelper.ts
@@ -0,0 +1,11 @@
+﻿/// <reference path="../jquery/jquery.d.ts"/>
+/// <reference path="../es6-promise/es6-promise.d.ts"/>
+
+class NavbarHelper {
+
+    constructor() {
+    }
+
+    public asacsccas(): void {
+    }
+}
